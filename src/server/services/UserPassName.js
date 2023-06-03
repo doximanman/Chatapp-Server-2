63c03120f1c99a1eb40b5a1e18e07b358cf2c536
EinatNoyman <einat2004@gmail.com>
2023-06-03 22:55:45 +0300

    Add changes

--- src/server/services/UserPassName.js
+++ src/server/services/UserPassName.js
@@ -1,8 +1,8 @@
-const User = require('../models/UserPassName')
+const UserPassName = require('../models/UserPassName')
 
 const createUserPassName = async (username, password,  displayName, profilePic) => {
-    const user = new User({ username: username, password: password, displayName: displayName, profilePic: profilePic });
-    return await user.save();
+    const userPassName = new UserPassName({ username: username, password: password, displayName: displayName, profilePic: profilePic });
+    return await userPassName.save();
 };
 
 // const getUserByUsername = async (username) => {
